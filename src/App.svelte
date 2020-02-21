d9e6268d280b561c4711b5368e2369551d9a9db5
Kanishk Agrawal <kanishk.agrawal@flipkart.com>
2020-02-21 12:08:39 +0530

    use $ labels to change variable

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
 	export let name;
 	let age = 24;
+	$: upperCaseName = name.toUpperCase();
+
 	function changeAge() {
 		age = age + 1;
 	}
+
+	function changeName() {
+		name = 'Kanishk Agrawal';
+	}
+
 </script>
 
 <style>
@@ -12,6 +19,7 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {upperCaseName}!</h1>
 <h2>My age is {age}</h2>
 <button on:click='{changeAge}'>Change Age</button>
+<button on:dblclick='{changeName}'>Change Name by Double Click</button>
