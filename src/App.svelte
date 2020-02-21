998690aa39b89e0e2dcccd2b86c8d149d9378874
Kanishk Agrawal <kanishk.agrawal@flipkart.com>
2020-02-21 11:58:46 +0530

    svelte first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+	let age = 24;
+	function changeAge() {
+		age = age + 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<h2>My age is {age}</h2>
+<button on:click='{changeAge}'>Change Age</button>
